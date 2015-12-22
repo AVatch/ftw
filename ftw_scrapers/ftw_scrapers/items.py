16d3081357c72ef28ce01cbc0fa9d43d7136c4af
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-12-22 18:22:05 -0500

    fixed errors wiht whitespace

--- ftw_scrapers/ftw_scrapers/items.py
+++ ftw_scrapers/ftw_scrapers/items.py
@@ -2,15 +2,15 @@ import scrapy
 
 
 class Game(scrapy.Item):
-	kind = scrapy.Field() 
-    title = scrapy.Field()
-    artwork = scrapy.Field()
-    achievement_count = scrapy.Field()
-    achievement_list_url = scrapy.Field()
+	kind = scrapy.Field()
+	title = scrapy.Field()
+	artwork = scrapy.Field()
+	achievement_count = scrapy.Field()
+	achievement_list_url = scrapy.Field()
 
 
 class Achievement(scrapy.Item):
-	kind = scrapy.Field() 
+	kind = scrapy.Field()
 	game = scrapy.Field()
 	title = scrapy.Field()
 	artwork = scrapy.Field()
